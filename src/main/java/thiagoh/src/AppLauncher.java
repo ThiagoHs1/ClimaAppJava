079a7987ce485411c293500af5bf578c87822f96
Thiago Henrique Heinle Sanchez <thiagohenque@live.com>
2024-09-09 19:24:42 -0300

    First Commit

---
+++ src/main/java/thiagoh/src/AppLauncher.java
@@ -0,0 +1,14 @@
+package thiagoh.src;
+
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClimaAppGUI().setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/main/java/thiagoh/src/ClimaAppGUI.java
@@ -0,0 +1,17 @@
+package thiagoh.src;
+
+import javax.swing.*;
+
+public class ClimaAppGUI extends JFrame {
+
+    public ClimaAppGUI() {
+        super("Clima");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(300, 200);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+    }
+}
